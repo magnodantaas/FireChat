5acee701fb88ded2c7ca64c395aa76069319fd9b
Magno Miranda Dantas <magnodantas1991@gmail.com>
2021-06-16 20:56:10 -0300

    Initial Commit

---
+++ FireChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FireChat
+//
+//  Created by Magno Miranda Dantas on 16/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
