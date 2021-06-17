29eeb8aea7aa908ae0bf671857aa694b06198ab0
Magno Miranda Dantas <magnodantas1991@gmail.com>
2021-06-16 21:55:57 -0300

    Adding tableView to message list page

--- FireChat/Controller/ConversationsControllers.swift
+++ FireChat/Controller/ConversationsControllers.swift
@@ -5,4 +5,93 @@
 //  Created by Magno Miranda Dantas on 16/06/21.
 //
 
-import Foundation
+import UIKit
+
+private let reuseIdentifier = "ConversationCell"
+
+class ConversationsController: UIViewController {
+    
+    // MARK: - Properties
+    
+    private let tableView = UITableView()
+    
+    // MARK: - LifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    // MARK: - Selectors
+    
+    @objc func showProfile() {
+        
+    }
+    
+    // MARK: - Helpers
+    
+    func configureUI() {
+        view.backgroundColor = .white
+
+        configureNavigationBar()
+        configureTableView()
+        
+        let image = UIImage(systemName: "person.circle.fill")
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .plain,
+                                                           target: self, action: #selector(showProfile))
+    }
+    
+    func configureTableView() {
+        tableView.backgroundColor = .white
+        tableView.rowHeight = 80
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.tableFooterView = UIView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        view.addSubview(tableView)
+        tableView.frame = view.frame
+    }
+    
+    func configureNavigationBar() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        appearance.backgroundColor = .systemPurple
+        
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "Messages"
+        navigationController?.navigationBar.tintColor = .white
+        navigationController?.navigationBar.isTranslucent = true
+        
+        navigationController?.navigationBar.overrideUserInterfaceStyle = .dark
+        
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension ConversationsController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        cell.textLabel?.text = "Test Cell"
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+
+extension ConversationsController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
+
+}
